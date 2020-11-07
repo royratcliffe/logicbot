2a2e141b56f6abb2779828ac9990bc87bebd9f09
Roy Ratcliffe <royratcliffe@me.com>
2020-11-07 10:52:34 +0000

    Rename logicBot to bot

--- src/test/kotlin/io/github/royratcliffe/logic/BotTest.kt
+++ src/test/kotlin/io/github/royratcliffe/logic/BotTest.kt
@@ -11,41 +11,41 @@ import kotlin.test.assertTrue
 class BotTest {
     @Test
     fun `Open and close`() {
-        val logicBot = Bot.Factory.newInstance()
-        assertFalse(logicBot.isClosed())
-        logicBot.assertZ("a", 1)
-        logicBot.assertZ("a", 2)
-        logicBot.assertZ("a", "hello")
-        val substitutions = logicBot.solve("a(Hello)", Duration.ofSeconds(1)).map {
+        val bot = Bot.Factory.newInstance()
+        assertFalse(bot.isClosed())
+        bot.assertZ("a", 1)
+        bot.assertZ("a", 2)
+        bot.assertZ("a", "hello")
+        val substitutions = bot.solve("a(Hello)", Duration.ofSeconds(1)).map {
             it.substitutionsAsMap()
         }
         assertEquals(3, substitutions.size)
         assertEquals(mapOf("Hello" to 1).toString(), substitutions[0].toString())
         assertEquals(mapOf("Hello" to 2).toString(), substitutions[1].toString())
         assertEquals(mapOf("Hello" to "hello").toString(), substitutions[2].toString())
-        logicBot.close()
-        assertTrue(logicBot.isClosed())
+        bot.close()
+        assertTrue(bot.isClosed())
         // Sleep for one second before asserting closure once more. This gives
         // time for the bot to finish closing and helps test coverage. Without
         // the delay, the test quits before the bot implementation's actor scope
         // returns.
         Thread.sleep(1000)
-        assertTrue(logicBot.isClosed())
+        assertTrue(bot.isClosed())
     }
 
     @Test
     fun `Retract dynamic knowledge`() {
-        val logicBot = Bot.Factory.newInstance()
-        assertEquals(0, logicBot.solve("a(X)", Duration.ofSeconds(1)).size)
-        logicBot.assertZ("a", 1)
-        assertEquals(1, logicBot.solve("a(X)", Duration.ofSeconds(1)).size)
-        logicBot.assertZ("a", 2)
-        assertEquals(2, logicBot.solve("a(X)", Duration.ofSeconds(1)).size)
-        logicBot.assertZ("a", "hello")
-        assertEquals(3, logicBot.solve("a(X)", Duration.ofSeconds(1)).size)
-        logicBot.retractAll("a", "_")
-        assertEquals(0, logicBot.solve("a(X)", Duration.ofSeconds(1)).size)
-        logicBot.close()
+        val bot = Bot.Factory.newInstance()
+        assertEquals(0, bot.solve("a(X)", Duration.ofSeconds(1)).size)
+        bot.assertZ("a", 1)
+        assertEquals(1, bot.solve("a(X)", Duration.ofSeconds(1)).size)
+        bot.assertZ("a", 2)
+        assertEquals(2, bot.solve("a(X)", Duration.ofSeconds(1)).size)
+        bot.assertZ("a", "hello")
+        assertEquals(3, bot.solve("a(X)", Duration.ofSeconds(1)).size)
+        bot.retractAll("a", "_")
+        assertEquals(0, bot.solve("a(X)", Duration.ofSeconds(1)).size)
+        bot.close()
     }
 
     @Test
