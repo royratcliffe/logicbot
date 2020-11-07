7cbeec1842b3c3bc3cd56c8b49ec78d0d60f7c38
Roy Ratcliffe <royratcliffe@me.com>
2020-11-07 10:12:36 +0000

    Load static knowledge test

--- src/test/kotlin/io/github/royratcliffe/logic/BotTest.kt
+++ src/test/kotlin/io/github/royratcliffe/logic/BotTest.kt
@@ -47,4 +47,37 @@ class BotTest {
         assertEquals(0, logicBot.solve("a(X)", Duration.ofSeconds(1)).size)
         logicBot.close()
     }
+
+    @Test
+    fun `Load static knowledge`() {
+        val bot = Bot.Factory.newInstance()
+        bot.loadStaticKb("add(A, B, C) :- C is A + B.")
+        bot.solve("add(1, 2, Int)", Duration.ofSeconds(1)).map {
+            it.substitutionsAsMap()
+        }.also {
+            assertEquals(1, it.size)
+            it.first()["Int"].also { actual ->
+                assertTrue(actual is Int)
+                assertEquals(3, actual)
+            }
+        }
+        bot.solve("A is 1 << 31, add(A, A, Long)", Duration.ofSeconds(1)).map {
+            it.substitutionsAsMap()
+        }.also {
+            assertEquals(1, it.size)
+            it.first()["Long"].also { actual ->
+                assertTrue(actual is Long)
+                assertEquals(4294967296, actual)
+            }
+        }
+        bot.solve("add(1.1, 2.2, Real)", Duration.ofSeconds(1)).map {
+            it.substitutionsAsMap()
+        }.also {
+            assertEquals(1, it.size)
+            it.first()["Real"].also { actual ->
+                assertTrue(actual is Double)
+                assertEquals(3.3, actual)
+            }
+        }
+    }
 }
