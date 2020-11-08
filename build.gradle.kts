9c86510dba275edd6e9b14c4f73badbc88f4282b
Roy Ratcliffe <royratcliffe@me.com>
2020-11-08 09:26:19 +0000

    Publish to GitHubPackages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.10"
+    `maven-publish`
 }
 
 group = "io.github.royratcliffe"
@@ -16,4 +17,17 @@ dependencies {
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "latest.release")
     implementation("it.unibo.tuprolog", "dsl-solve-jvm", "latest.release")
     implementation("it.unibo.tuprolog", "parser-theory", "latest.release")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/royratcliffe/logicbot")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
